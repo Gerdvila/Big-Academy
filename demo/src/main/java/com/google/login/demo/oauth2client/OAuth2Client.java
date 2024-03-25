4c52c0327e5447188c5473bcd45c0f399cede276
Edvinas <edvinas.gerdvila@mif.stud.vu.lt>
2024-03-25 19:45:18 +0200

    1

--- demo/src/main/java/com/google/login/demo/oauth2client/OAuth2Client.java
+++ demo/src/main/java/com/google/login/demo/oauth2client/OAuth2Client.java
@@ -1,10 +1,4 @@
 package com.google.login.demo.oauth2client;
-
-//import spring.org.javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
